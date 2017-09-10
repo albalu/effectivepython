9bb147dd02e9badd7e6a8e46227ad4e77ffe6f05
Alireza <alireza@wustl.edu>
2017-09-10 16:32:40 -0700

    clarifying prints and comments

--- example_code/item_56.py
+++ example_code/item_56.py
@@ -23,9 +23,32 @@ from sys import stdout as STDOUT
 # Example 3
 from tempfile import TemporaryDirectory
 from unittest import TestCase
+
+
 class MyTest(TestCase):
     def setUp(self):
         self.test_dir = TemporaryDirectory()
+        print()
+        print('test setUp...')
+        print('Use setUp and teardown only when you need to have some'
+              'special environment (e.g. a temp folder like follows) setup!')
+        print('temporary directory: {}'.format(self.test_dir))
+
     def tearDown(self):
         self.test_dir.cleanup()
+        print()
+        print('test tearDown...')
+
     # Test methods follow
+    def test(self):
+        print()
+        print('the actual test here...')
+
+    # note how naming the functions matter!
+    def some_other_test(self):
+        print()
+        print('this test does not run!')
+
+    def test_other(self):
+        print()
+        print('test number 2 ...')
